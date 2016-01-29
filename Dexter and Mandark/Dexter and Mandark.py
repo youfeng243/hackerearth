6d86875b03aeef9cd0302bb867afecb3b26d6684
youfeng <youfeng243@163.com>
2016-01-29 22:20:08 +0800

    做题提交

--- Dexter and Mandark/Dexter and Mandark.py
+++ Dexter and Mandark/Dexter and Mandark.py
@@ -4,13 +4,18 @@ def main():
     
     for _ in xrange(T):
         N,M = map(int, raw_input().strip().split())
+        
         DP = [0] * (N + 1)
-        DP[M] = 1
-        for i in xrange( M + 1, N + 1 ):
-            DP[i] = DP[i - 1] * 2
-            DP[i] %= (10**9 + 7)
-        #DP[N] %= (10 ** 9 + 7)
-        print DP[N]
+        if M > N / 2:  
+            DP[M] = 1
+            for i in xrange( M + 1, N + 1 ):
+                DP[i] = DP[i - 1] * 2
+                DP[i] %= (10**9 + 7)
+            print DP[N]
+            continue
+        
+        
+        
         
 if __name__=="__main__":
     main()
